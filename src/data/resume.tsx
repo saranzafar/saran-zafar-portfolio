1f4491c7393f306a93d2327f247754604acc19e5
saranzafar <saran.development@gmail.com>
2025-01-14 10:12:51 +0500

    update blog

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -32,7 +32,7 @@ export const DATA = {
   ],
   navbar: [
     { href: "/", icon: HomeIcon, label: "Home" },
-    { href: "/blog/read", icon: NotebookIcon, label: "Blog" },
+    { href: "/blog", icon: NotebookIcon, label: "Blog" },
   ],
   contact: {
     email: "saran.development@gmail.com",
